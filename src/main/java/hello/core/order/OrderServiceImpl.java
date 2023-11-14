31abcaee8be14e2eab522d9af0cf553649c60bf4
osg9010 <osg3419010@gmail.com>
2023-11-14 21:53:58 +0900

    Revert "중복된 타입의 빈이 여러 개 있을 경우 @Qualifier @Primary 어노테이션 등을 이용한 방법으로 어떤것을 사용할지 정할 수 있다."
    This reverts commit bbbfd51d

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,6 +1,5 @@
 package hello.core.order;
 
-import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
@@ -9,10 +8,10 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     //LINE:: DIP 위배 추상이 아니라 구체화에 의존하기때문.
@@ -33,10 +32,10 @@ public class OrderServiceImpl implements OrderService{
 
 
     //@Autowired  // LINE :: 생성자가 단 하나 있을 때는 생략가능 , Lombok @RequiredArgsConstructor
-    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
