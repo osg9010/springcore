7b4a59f704b3a22c4ad2ba6780daeeed9eec4e90
osg9010 <osg3419010@naver.com>
2023-11-14 22:04:11 +0900

    중복된 타입의 빈이 여러 개 있을 경우 @Qualifier @Primary 어노테이션 등을 이용한 방법으로 어떤것을 사용할지 정할 수 있다.
    우선순위 Qualifier > Primary

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("fixB") // LINE :: 명시적으로 지정할 수 있게 하는 어노테이션 primary 보다 우선권이 높다
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000; // 1000원할인
 
