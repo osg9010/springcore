2c5208a647ae813f2b886eca393df19b6864a849
osg9010 <osg3419010@naver.com>
2023-11-14 22:04:48 +0900

    중복된 타입의 빈이 여러 개 있을 경우 @Qualifier @Primary 어노테이션 등을 이용한 방법으로 어떤것을 사용할지 정할 수 있다.
    우선순위 Qualifier > Primary

---
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,15 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+    // LINE :: 다양한 상황 일 경우 어노테이션을 직접 만들어서 사용 가능하다.
+
+}
