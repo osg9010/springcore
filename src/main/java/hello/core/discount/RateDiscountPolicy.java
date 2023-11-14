bbbfd51d09dc7c72a6762bb10715d1f847609b7e
osg9010 <osg3419010@gmail.com>
2023-11-14 21:44:57 +0900

    중복된 타입의 빈이 여러 개 있을 경우 @Qualifier @Primary 어노테이션 등을 이용한 방법으로 어떤것을 사용할지 정할 수 있다.
    우선순위 Qualifier > Primary

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,10 +1,14 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Primary // LINE :: 여러 타입의 같은 빈이 있을 경우 기본 주입되는 빈으로 등록
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
