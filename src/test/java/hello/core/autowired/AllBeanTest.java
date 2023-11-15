3ad760ca54a59cab22108c1f608bf4732ce4a967
osg9010 <osg3419010@naver.com>
2023-11-15 17:22:26 +0900

    빈으로 등록한 클래스들은 타입이 같을 경우 상위 타입으로 여러개의 빈들을 맵이나 리스트 등을 이용해 받아올 수 있다.
    맵을 이용해 가져오면 빈들에 다형성을 적용하기 쉬워진다.

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,51 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import lombok.RequiredArgsConstructor;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class,DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+
+        Member member = new Member(1L,"userA", Grade.VIP);
+        int discountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+        Assertions.assertThat(discountService).isInstanceOf(DiscountService.class);
+        Assertions.assertThat(discountPrice).isEqualTo(1000);
+
+        int ratePrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+        Assertions.assertThat(ratePrice).isEqualTo(2000);
+
+    }
+
+    static class  DiscountService{
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+            this.policyMap = policyMap;
+            this.policyList = policyList;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policyList = " + policyList);
+        }
+
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policyList;
+
+        public int discount(Member member, int i, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+
+            return discountPolicy.discount(member,i);
+        }
+    }
+}
