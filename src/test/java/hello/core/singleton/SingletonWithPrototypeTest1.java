4035aa48cf4e53c65269cdb884ed949801fca4c8
osg9010 <osg3419010@naver.com>
2023-11-17 15:42:09 +0900

    @Scope("singleton")에서 @Scope("prototype") 스코프의 빈을 사용하려면 ObjectFactory, ObjectProvider, Provider를 사용해야 한다.
    ObjectFactory, ObjectProvider 는 스프링에서 제공하는 인터페이스로
    getObject() 메소드를 통해 @Scope("prototype") 스코프의 빈을
    주입받아 사용할 수 있다.
    
    Provider는 자바 표준으로 get() 메소드를 통해 주입받을 수 있다.
    표준이긴 하지만 gladle 을 통해 디펜던시를 임포트 해와야함.

--- src/test/java/hello/core/singleton/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/singleton/SingletonWithPrototypeTest1.java
@@ -2,12 +2,15 @@ package hello.core.singleton;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 public class SingletonWithPrototypeTest1 {
 
@@ -33,19 +36,32 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean bean2 = ac.getBean(ClientBean.class);
         int count2 = bean2.logic();
-        Assertions.assertThat(count2).isEqualTo(2);
+        Assertions.assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; // LINE :: 생성 시점에 주입 됌 - 프로토 타입 빈 이여도 생성 시점에 주입 됌
+
+//        private final PrototypeBean prototypeBean; // LINE :: 생성 시점에 주입 됌 - 프로토 타입 빈 이여도 생성 시점에 주입 됌
+        //        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean){
+//            this.prototypeBean = prototypeBean;
+//        }
+
+//        @Autowired
+//        private ObjectFactory<PrototypeBean> prototypeBeanObjectFactory;
+
+//        @Autowired
+//        private ObjectProvider<PrototypeBean> prototypeBeanObjectProvider;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        Provider<PrototypeBean> prototypeBeanProvider;
+
 
         public int logic(){
+//            PrototypeBean prototypeBean = prototypeBeanObjectFactory.getObject();
+//            PrototypeBean prototypeBean = prototypeBeanObjectProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
