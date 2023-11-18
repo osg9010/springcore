f7c6f614be55c234ffa2386f74fd5c5f618aafe1
osg9010 <osg3419010@naver.com>
2023-11-18 13:38:37 +0900

    MyLogger 클래스의 스코프가 request 라서
    Error creating bean with name 'myLogger': Scope 'request' is not active for the current thread; consider defining a scoped proxy for this bean if you intend to refer to it from a singleton;
    
    의존성을 주입 받으면 이 에러가 뜨는데 해당 에러를
    ObjectProvider.getObject() 메소드를 통해 해결 하였다.

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +15,19 @@ import java.net.http.HttpRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+//    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
-    public String logDemo(HttpServletRequest request){
+    public String logDemo(HttpServletRequest request) throws InterruptedException {
         String requestUrl = request.getRequestURL().toString();
+
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestUrl);
         myLogger.log("controller Test");
+
+        Thread.sleep(1000);
         logDemoService.logic("testId");
         return "ok";
     }
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -2,15 +2,19 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+//    private final MyLogger myLogger;
+
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     public void logic(String testId) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("serviceId : " + testId);
     }
 }
